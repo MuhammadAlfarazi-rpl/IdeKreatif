cd8cba889a01679e7ae7a3d1206fe61371dc5398
Riis / Alfarazi <98535387+MuhammadAlfarazi-rpl@users.noreply.github.com>
2025-02-10 11:21:43 +0700

    Menambahkan Konten Di Halaman Post

---
+++ posts.php
@@ -0,0 +1,62 @@
+<?php
+
+include '.includes/header.php';
+
+?>
+
+<div class="container-xxl flex-grow-1 container-p-y">
+    <div class="row">
+        <div class="col-md-10">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <form method="POST" action="proses_post.php" enctype="multipart/form-data">
+                        <!-- Judul -->
+                         <div class="mb-3">
+                            <label for="post_title" class="form-label">Judul Postingan</label>
+                            <input type="text" class="form-control" name="post_title" required>
+                         </div>
+
+                         <!-- Input upload Gambar -->
+                          <div class="mb-3">
+                            <label for="formFile" class="form-label">Unggah</label>
+                            <input class="form-control" type="file" name="image" accept="image/*" /> 
+                        </div>
+
+                        <!-- Dropdown kategori -->
+                        <div class="mb-3">
+                        <label for="category_id" class="form-label">Kategori</label>
+                        <select class="form-select" name="category_id" required>
+                            <!-- Mengambil data kategori untuk dropdown -->
+                             <option value="" selected disabled>Pilih salah satu</option>
+
+                            <?php
+
+                            $query = "SELECT * FROM categories";
+                            $result = $conn->query($query);
+                            if ($result->num_rows > 0) {
+                                    while ($row = $result->fetch_assoc()) {
+                                        echo "<option> value='". $row["category_id"] . "'>" . $row["category_name"] . "</option>";
+                                    }
+                                }
+
+                            ?>
+                        </select>
+                        </div>
+
+                        <!-- Textarea Postingan -->
+                         <div class="mb-3">
+                            <label for="content" class="form-label">Konten</label>
+                            <textarea class="form-control" id="content" name="content"required></textarea>
+                         </div>
+                         <!-- Submit -->
+                          <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+                    </form>
+                </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<?php
+include '.includes/footer.php';
+?>
